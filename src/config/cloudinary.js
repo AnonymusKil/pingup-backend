97c42c3d4cec456ecac568906015a336beb30a0b
Anonymuskil <navadesignz11@gmail.com>
2026-09-07 00:50:47 -0700

    fixed

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -5,10 +5,5 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_SECERET_API_KEY,
 });
-console.log("Cloudinary ENV:", {
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_SECERET_API_KEY,
-});
 
 export default cloudinary;
